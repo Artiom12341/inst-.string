0885063b8aa04b41e0950cefee3b8ae76bc7e943
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-11-24 02:09:57 +0300

    Add files via upload

---
+++ informatica.inst.str.3.py
@@ -0,0 +1,10 @@
+n=str(input("dati cuvantul-"))
+a=str(input("dati cuvantul-"))
+b=str(input("dati cuvantul-"))
+c=str(input("dati cuvantul-"))
+s1=n[:2]
+s2=a[:1]
+s3=b[:3]
+d=len(c)//2
+s4=c[:d]
+print(s1+s2+s3+s4)
